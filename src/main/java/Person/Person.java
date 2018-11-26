5e4429f93e8c0b886c3a54c834639678ca015304
Denis <Denbond95@mail.ru>
2018-11-26 15:36:54 +0300

    Massiv with ass,set and remove methods

--- src/main/java/Person/Person.java
+++ src/main/java/Person/Person.java
@@ -1,21 +1,40 @@
 package Person;
 
-import java.util.Scanner;
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
 
-public class Person {
-    public static void main(String[] args) {
-        String name = EnterName();
+class Person {
+    private String name;
+    private String gender;
+    private int id;
+    private int age;
+
+
+    public Person(String name, String gender, int id, String birthDate) {
+        this.name = name;
+        this.gender = gender;
+        this.id = id;
+        LocalDate now = new LocalDate();
+        LocalDate birthDates = new LocalDate(birthDate);
+        age = Years.yearsBetween(birthDates, now).getValue(0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
     }
 
 
-    public static String EnterName() {
-        {
-            System.out.print("Введите имя: ");
-            Scanner scan = new Scanner(System.in);
-            String namePerson = scan.nextLine();
-            return namePerson;
-
-        }
-    }
 }
 
---
+++ src/main/java/Person/PersonRepository.java
@@ -0,0 +1,36 @@
+package Person;
+
+import java.util.ArrayList;
+
+class PersonRepository {
+    private ArrayList<Person> PersonRepository = new ArrayList<Person>();
+
+    public PersonRepository() {
+        PersonRepository.add(new Person("Petrov Dmitry", "male", 1, "2001-02-12"));
+        PersonRepository.add(new Person("Sviridov Artem", "male", 2, "2003-09-11"));
+        PersonRepository.add(new Person("Petrov Petr", "male", 3, "2002-02-12"));
+        PersonRepository.add(new Person("Fructov Orange", "male", 4, "1976-04-25"));
+        PersonRepository.add(new Person("Dimitrova Svetlana", "female", 5, "2002-11-09"));
+    }
+
+    public void setPersonInRepository(int index, String name, String gender, int id, String birthDate) {
+        PersonRepository.set(index, new Person(name, gender, id, birthDate));
+    }
+
+    public void addPersonInRepository(String name, String gender, int id, String birthDate) {
+        PersonRepository.add(new Person(name, gender, id, birthDate));
+    }
+
+    public void removePersonFromRepository(int index) {
+        PersonRepository.remove(index);
+    }
+
+    public void showPersonRepository() {
+        for (int i = 0; i < PersonRepository.size(); i++) {
+            System.out.println("Peson name: " + PersonRepository.get(i).getName() + "\nGender: " + PersonRepository.get(i).getGender() + "\nID: " + PersonRepository.get(i).getId() + "\nAge: " + PersonRepository.get(i).getAge() + "\n");
+        }
+    }
+}
+
+
+
